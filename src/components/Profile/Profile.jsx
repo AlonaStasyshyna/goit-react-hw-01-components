6f60af7a1e5555567524e01d7d80942eaff82bcd
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-10-03 22:27:15 +0300

    Add part of task 1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+export const Profile = () => {
+    return (
+        <div class="profile">
+
+            <ul class="stats">
+                <li>
+                <span class="label">Followers</span>
+                <span class="quantity">1000</span>
+                </li>
+                <li>
+                <span class="label">Views</span>
+                <span class="quantity">2000</span>
+                </li>
+                <li>
+                <span class="label">Likes</span>
+                <span class="quantity">3000</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
