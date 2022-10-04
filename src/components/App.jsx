a5b7461176fc1703a0f1e358203e5437ea61d17e
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2022-10-04 18:23:48 +0300

    Add task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import { Profile } from "./Profile/Profile";
+import userData from "../components/data/user.json";
 
 export const App = () => {
   return (
     <>
-      <Profile/>
+      <Profile user={userData} />
     </>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,47 @@
 import PropTypes from 'prop-types';
+import { Card, Avatar, Name, Text, List, ListItem, SpanName, SpanQuantity } from './Profile.styled';
 
-export const Profile = () => {
+export const Profile = ({user: {username, tag, location, avatar, stats}}) => {
     return (
-        <div class="profile">
+        <Card>
+            <div>
+                <Avatar
+                src={avatar}
+                alt={username}
+                />
+                <Name>{username}</Name>
+                <Text>@{tag}</Text>
+                <Text>{location}</Text>
+            </div>
 
-            <ul class="stats">
-                <li>
-                <span class="label">Followers</span>
-                <span class="quantity">1000</span>
-                </li>
-                <li>
-                <span class="label">Views</span>
-                <span class="quantity">2000</span>
-                </li>
-                <li>
-                <span class="label">Likes</span>
-                <span class="quantity">3000</span>
-                </li>
-            </ul>
-        </div>
+            <List>
+                <ListItem>
+                <SpanName>Followers</SpanName>
+                <SpanQuantity>{stats.followers}</SpanQuantity>
+                </ListItem>
+                <ListItem>
+                <SpanName>Views</SpanName>
+                <SpanQuantity>{stats.views}</SpanQuantity>
+                </ListItem>
+                <ListItem>
+                <SpanName>Likes</SpanName>
+                <SpanQuantity>{stats.likes}</SpanQuantity>
+                </ListItem>
+            </List>
+        </Card>
     )
+}
+
+Profile.propTypes = {
+    user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        tag: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
+        avatar: PropTypes.string.isRequired,
+        stats: PropTypes.shape({
+            followers: PropTypes.number.isRequired,
+            views: PropTypes.number.isRequired,
+            likes: PropTypes.number.isRequired,
+        }).isRequired
+    }).isRequired
 }
